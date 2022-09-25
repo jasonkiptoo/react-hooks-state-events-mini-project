125d16d55bd9fa328cd503d082e601c12e040582
jasonkiptoo <kiptoojason@gmail.com>
2022-09-25 11:26:57 +0300

    completed assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -2,25 +2,30 @@ import React, { useState } from "react";
 import { CATEGORIES } from "../data";
 
 function NewTaskForm({ onFilterTask }) {
-  const [categories] = useState(CATEGORIES);
-
+  const [text, setText] = useState("");
   const categoryList = CATEGORIES.map((option) => <option>{option}</option>);
 
-  // const handleFilterChange = () => {
-  //   onFilterTask(categories);
-  // };
+  function handleAddTask(e, text) {
+    console.log(text);
+    e.preventDefault();
+    setText("");
+  }
 
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleAddTask}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          onChange={(e) => console.log(e.target.value)}
+        />
       </label>
       <label>
         Category
         <select name="category">{categoryList}</select>
       </label>
-      <input type="submit" value="Add task" />
+      <input type="submit" />
     </form>
   );
 }
